5620fded04244f241b22c00782447b517a699975
Pau Rius <96046002+PauR03@users.noreply.github.com>
2022-01-21 12:00:14 +0100

    Add files via upload

--- M3/Parte jugable.py
+++ M3/Parte jugable.py
@@ -3,14 +3,32 @@ import pymysql
 conn = pymysql.connect(host="20.73.187.218", user="prius", password="P@ssw0rd", db="proyecto")
 cur = conn.cursor()
 
+def formatText(texto,longitud):
+    y = ""
+    z = ""
+    for i in texto:
+        y += i
+        if len(y) >= longitud and i.isspace():
+            z += y + "\n".ljust(6)
+            y = ""
+    z += y
+    return z
+
 aventura = input("Dime que aventura queires jugar: ")
 num_paso = 1
 while True:
     query = f"select * from pasos where num_paso = '{num_paso}'"
     cur.execute(query)
     pasos = cur.fetchone()
-    print(pasos[0],"-",pasos[1])
+    print(pasos[0],"-",formatText(pasos[1],100))
     if pasos[4] == 0:
+        print(" #######    ###    #     #\n",
+              "#           #     ##    #\n",
+              "#           #     # #   #\n",
+              "#####       #     #  #  #\n",
+              "#           #     #   # #\n",
+              "#           #     #    ##\n",
+              "#          ###    #     #")
         break
     input()
     query = f"select * from respuesta where id_paso = '{pasos[0]}'"
